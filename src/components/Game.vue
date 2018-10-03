c3c02e25278c4e970528d55778be042b7c2e392f
Kenneth Tran <quoc.tran@okstate.edu>
2018-10-03 00:09:51 -0500

    finished

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="container" id="content">
-    <div id="progress-bar" style="visibility: hidden;">
-      <v-progress-linear 
-      :value="value"
-      background-color="blue-grey"
-      color="lime"
-      height="10"
-      style="border-radius: 12px"
-      >
-      {{ value }}
-    </v-progress-linear >
-</div>
+    <div id="progress-bar">
+      <div id="bar" v-bind:style="{ width: value + '%'}">
+    </div>
+  </div>
     <div class="top">
       <div id="score" style="visibility: hidden;">Score {{ this.score }}</div>
       <div id="resetBtn"><v-btn id="reset" v-on:click="restart" icon color="success" style="visibility: hidden;"><v-icon>cached</v-icon></v-btn></div>
@@ -21,57 +14,66 @@
       <div id="equation"></div>
       <div class="btn">
         <span>
-          <v-btn depressed large id="btn1" style="visibility: hidden;" v-on:click="trueClicked"><v-icon>done</v-icon></v-btn>
-          <v-btn depressed large id="btn2" style="visibility: hidden;" v-on:click="falseClicked"><v-icon>clear</v-icon></v-btn>
+          <v-btn depressed x-large id="btn1" style="visibility: hidden;" v-on:click="trueClicked"><v-icon>done</v-icon></v-btn>
+          <v-btn depressed x-large id="btn2" style="visibility: hidden;" v-on:click="falseClicked"><v-icon>clear</v-icon></v-btn>
         </span>
       </div>
       <div class="status">
-        <div id="status"></div>
+        <div id="blinking" style="visibility: hidden; color: #DC4C46" v-on:click="restart">GAME OVER</div>
       </div>
-      <v-btn depressed class="btn" v-on:click="restart" style="visibility: hidden;" id="reset">Click To Restart</v-btn>
     </div>
   </div>
 </template>
 
 <script>
 var compareSums;
-var backgroundColor = ["#F39C12", "#8E44AD", "#27AE60", "#16A085", "#7F8C8D", "#006E6D", "#BE9EC9"];
-var progressBar, progressTimer, timeOut, outOfTime;
+var backgroundColor = [
+  "#F39C12",
+  "#8E44AD",
+  "#27AE60",
+  "#16A085",
+  "#7F8C8D",
+  "#006E6D",
+  "#482e51"
+];
+var timeInterval;
 
 export default {
   name: "Game",
   data() {
     return {
       score: 0,
-      interval: {},
       value: 0
     };
   },
-  beforeDestroy () {
-      clearInterval(this.interval)
-    },
+  // beforeDestroy () {
+  //     clearInterval(this.timeInterval)
+  //   },
   methods: {
     restart() {
+      document.getElementById("blinking").style.visibility = "hidden";
       this.score = 0;
       this.game();
-      document.getElementById("reset").style.visibility = "hidden";
     },
     gameStart() {
       this.setBackgroundColor();
       document.getElementById("startBtn").style.visibility = "hidden";
       document.getElementById("btn1").style.visibility = "visible";
       document.getElementById("btn2").style.visibility = "visible";
-      document.getElementById("score").style.visibility = "visible"; 
-      document.getElementById("progress-bar").style.visibility = "visible"; 
+      document.getElementById("score").style.visibility = "visible";
+      document.getElementById("bar").style.visibility = "visible";
       document.getElementById("reset").style.visibility = "visible";
-      document.getElementById("status").innerHTML = "";
     },
     game() {
-      this.countDownTimer();
-      console.log(this.score);
+      this.timeReset();
+      // console.log(this.score);
       this.gameStart();
-      var operands = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-      var deviation = [-1, 0, 2, 1, -3];
+      //var operands = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 13, 15, 20, 92, 99];
+      var operands = Array.from(
+        { length: 15 },
+        () => Math.floor(Math.random() * 36) + 1
+      );
+      var deviation = [-1, 0, 2, 1];
       var operand1 = operands[Math.floor(Math.random() * operands.length)];
       var operand2 = operands[Math.floor(Math.random() * operands.length)];
       var correctSum = operand1 + operand2;
@@ -88,15 +90,16 @@ export default {
 
       document.getElementById("equation").innerHTML = resultString;
       compareSums = correctSum == shownSum;
-      console.log(compareSums);
+      // console.log(compareSums);
     },
 
     gameOver: function() {
-      document.getElementById("status").innerHTML = "Game Over";
-      document.getElementById("status").style.color = "#DC4C46";
+      clearInterval(this.timeInterval);
+      document.getElementById("blinking").style.visibility = "visible";
       document.getElementById("reset").style.visibility = "visible";
       document.getElementById("btn1").style.visibility = "hidden";
       document.getElementById("btn2").style.visibility = "hidden";
+      document.getElementById("bar").style.visibility = "hidden";
       this.value = 0;
     },
 
@@ -117,20 +120,27 @@ export default {
       }
     },
     setBackgroundColor: function() {
-      var color = backgroundColor[Math.floor(Math.random() * backgroundColor.length)];
+      var color =
+        backgroundColor[Math.floor(Math.random() * backgroundColor.length)];
       document.getElementById("content").style.backgroundColor = color;
     },
-    countDownTimer: function() {
-      this.interval = setInterval(() => {
-        if (this.value == 100) {
-          clearInterval(this.interval)
-          this.gameOver();
-        } else {
-        this.value ++;
-      }}, 20);
-    },
+    timeReset: function() {
+      this.value = 100;
+      clearInterval(timeInterval);
+      var timeInterval = setInterval(() => {
+        this.value--;
+      }, 150);
+    }
+  },
+  watch: {
+    value: function(event) {
+      if (this.value == 0) {
+        clearTimeout(timeInterval);
+        this.gameOver();
+      }
+    }
   }
-}
+};
 </script>
 
 <style scoped>
@@ -167,8 +177,9 @@ body {
   font-size: 8px;
   padding-top: 5px;
 }
-#score, #resetBtn {
-    display: inline;
+#score,
+#resetBtn {
+  display: inline;
 }
 #equation {
   height: 53%;
@@ -184,7 +195,7 @@ body {
   border-radius: 12px;
 }
 #btn1:hover {
-  background-color: #4CAF50;
+  background-color: #4caf50;
   color: white;
   font-size: 25px;
   box-shadow: 0 9px #999;
@@ -204,7 +215,7 @@ body {
   box-shadow: 0 9px #999;
   border-radius: 12px;
 }
-#startBtn{
+#startBtn {
   background-color: #9068be;
   color: white;
   font-size: 20px;
@@ -212,7 +223,7 @@ body {
   box-shadow: 0 9px gray;
   border-radius: 18px;
 }
-#startBtn:hover{
+#startBtn:hover {
   background-color: #dddfd4;
   color: white;
   font-size: 20px;
@@ -220,8 +231,12 @@ body {
   box-shadow: 0 9px #999;
   border-radius: 18px;
 }
-.v-progress-linear{
-  margin: auto;
+#bar {
+  height: 10px;
+  background: darkred;
+  display: block;
+  width: 100%;
+  border-radius: 12px;
 }
 #score {
   font-size: 11pt;
@@ -236,5 +251,22 @@ body {
 .content {
   display: inline-block;
 }
+@keyframes blinking {
+  0% {
+    opacity: 0;
+    letter-spacing: 1px;
+  }
+  50% {
+    opacity: 0.5;
+    letter-spacing: 2px;
+  }
+  100% {
+    opacity: 1;
+    letter-spacing: 3px;
+  }
+}
 
+#blinking {
+  animation: blinking 1s infinite;
+}
 </style>
